771c007d0f497b37ab007c2d01eda254022e69f9
Skateone <sreynolds@skateworx.co.za>
2013-05-21 23:28:42 +0200

    fixed spelling

--- ITDB02_2_8_Shield_TEST/ITDB02_2_8_Shield_TEST.ino
+++ ITDB02_2_8_Shield_TEST/ITDB02_2_8_Shield_TEST.ino
@@ -1,25 +1,16 @@
 /*
 This sample application was written to test the functionality of the ITDB02-2.8 Display Shield
 It allows to check if the touch sensor is operation as well as check screen integrity
-
 This demo was put together the www.robotics.org.za 
-
 NOTE: you will need to downlaod the UFTF and Utouch libs from: 
 http://henningkarlsen.com/electronics/library.php?id=52
-
-
 Authored by Shaun Reynolds (The Skate)
-
 */
-
-
 #include <UTFT.h>
 #include <UTouch.h>
-
-
-//UFTF class initialization 
 /*
- UFTF(Model,RS,WR,CS,RST)
+UTFT class initialization 
+ UTFT(Model,RS,WR,CS,RST)
   Model = ITDB28
   RS    = A5
   WR    = A4
@@ -27,8 +18,6 @@ Authored by Shaun Reynolds (The Skate)
   RST   = A2
 */
 UTFT           myGLCD(ITDB28,A5,A4,A3,A2);   //UFTF class init 
-
-
 /*
  UTOUCH(TCLK, TCS, TDIN, TDOUT, IRQ)
  
@@ -44,7 +33,6 @@ boolean        touchLatch = false;          //touch latch varable
 long           x, y;                        //touch x and y vars
 long           oldX = 0, oldY = 0;          
 
-
 void setup()
 {
   myGLCD.InitLCD(LANDSCAPE);                //Init LCD as landscape
@@ -52,7 +40,6 @@ void setup()
   myTouch.InitTouch(LANDSCAPE);             //Init Touch as landscape
   myTouch.setPrecision(PREC_MEDIUM);        //Set touch precision to medium
 }
-
 //This function draws the cross that is displayed everytime you touch the screen
 //it uses the old position to erase the old cross and then redraws it to the new position
 void drawX() {
@@ -65,8 +52,6 @@ void drawX() {
   myGLCD.drawLine(x-5,y,x+5,y);
   myGLCD.drawLine(x,y-5,x,y+5);  
 }
-
-
 //Function to setup the basic layout of all the static elements
 void drawScreen() {
   myGLCD.setColor(255, 255, 255);
@@ -74,15 +59,12 @@ void drawScreen() {
   myGLCD.print("X:",173,220);
   myGLCD.print("Y:",250,220);
   myGLCD.setFont(SmallFont);
-
 }
-
 void loop()
 {
   drawScreen();
   while (myTouch.dataAvailable() == true)      //If touch is sensed then continue looping
   {
-    
     // this function applies latching so when you touch it will only drawe the red square 
     //once. the best way to work with LCD's is to only update what is needed on the display 
     //as writing to the display is quite a process intensive task
@@ -91,9 +73,7 @@ void loop()
       myGLCD.setColor(255, 0, 0);
       myGLCD.drawRect(170,217,317,237); 
     }
-    
     myTouch.read();                           //Read touch sensor
-    
     /*
     due to the layout of the ITDB02-2.8 display shield the x-axis is inverted when displaying and sensing in
     landscape mode.
@@ -104,7 +84,6 @@ void loop()
     y = myTouch.getY();
     if ((x!=-1) and (y!=-1))
     {
-      
       //updates the latest touch co-ordinates to the screen for easy reading
       myGLCD.setColor(255, 255, 255);
       myGLCD.setBackColor(0, 0, 0);
@@ -113,7 +92,6 @@ void loop()
       drawX() ;
     }
   }
-  
   //when the Touch is not active redraw the original white block once to indicate
   //no touch
   if(touchLatch) {
